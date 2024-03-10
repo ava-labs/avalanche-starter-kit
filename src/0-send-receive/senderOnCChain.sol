5aae4143900f4263f0962412c98c0f5aad170e6a
Martin Eckardt <martin.eckardt@avalabs.org>
2024-03-10 21:51:06 +0000

    Add example contracts

---
+++ src/0-send-receive/senderOnCChain.sol
@@ -0,0 +1,36 @@
+// (c) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// SPDX-License-Identifier: Ecosystem
+
+pragma solidity 0.8.18;
+
+import "@teleporter/ITeleporterMessenger.sol";
+
+contract SenderOnCChain {
+
+    ITeleporterMessenger public immutable teleporterMessenger = ITeleporterMessenger(0x253b2784c75e510dD0fF1da844684a1aC0aa5fcf);
+
+    /**
+     * @dev Sends a message to another chain.
+     */
+    function sendMessage(
+        address destinationAddress,
+        string calldata message
+    ) external returns (bytes32) {
+
+        return teleporterMessenger.sendCrossChainMessage(
+            TeleporterMessageInput({
+                destinationBlockchainID: 0x9f3be606497285d0ffbb5ac9ba24aa60346a9b1812479ed66cb329f394a4b1c7,
+                destinationAddress: destinationAddress,
+                feeInfo: TeleporterFeeInfo({
+                    feeTokenAddress: address(0),
+                    amount: 0
+                }),
+                requiredGasLimit: 100000,
+                allowedRelayerAddresses: new address[](0),
+                message: abi.encode(message, 16, true)
+            })
+        );
+    }
+}
