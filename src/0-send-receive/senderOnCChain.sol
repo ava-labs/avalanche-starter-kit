c68c800bb206b0ecbd4e0436009ee09cadf17088
Martin Eckardt <martin.eckardt@avalabs.org>
2024-03-26 20:21:54 +0000

    simplify SenderOnCChain

--- src/0-send-receive/senderOnCChain.sol
+++ src/0-send-receive/senderOnCChain.sol
@@ -8,17 +8,16 @@ pragma solidity ^0.8.18;
 import "@teleporter/ITeleporterMessenger.sol";
 
 contract SenderOnCChain {
-    ITeleporterMessenger public immutable teleporterMessenger =
-        ITeleporterMessenger(0x253b2784c75e510dD0fF1da844684a1aC0aa5fcf);
+    ITeleporterMessenger public immutable messenger = ITeleporterMessenger(0x253b2784c75e510dD0fF1da844684a1aC0aa5fcf);
 
     /**
      * @dev Sends a message to another chain.
      */
-    function sendMessage(address destinationAddress, string calldata message) external returns (bytes32) {
-        return teleporterMessenger.sendCrossChainMessage(
+    function sendMessage(address destinationAddress, string calldata message) external {
+        messenger.sendCrossChainMessage(
             TeleporterMessageInput({
                 // Replace with blockchainID of your Subnet (see instructions in Readme)
-                destinationBlockchainID: 0x92756d698399805f0088fc07fc42af47c67e1d38c576667ac6c7031b8df05293,
+                destinationBlockchainID: 0xa1adcd8b9ac1d5665494c4b8a830594b6486213aef8cfdb8beee61a31eae793f,
                 destinationAddress: destinationAddress,
                 feeInfo: TeleporterFeeInfo({feeTokenAddress: address(0), amount: 0}),
                 requiredGasLimit: 100000,
