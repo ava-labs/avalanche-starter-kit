653814a831f62fa4b2d9c8a643ea880c9504a086
owenwahlgren <owenwahlgren@gmail.com>
2024-10-01 19:23:04 +0000

    rm redundant dirs

--- contracts/interchain-token-transfer/ERC20.sol
+++ contracts/interchain-token-transfer/ERC20.sol
@@ -4,10 +4,10 @@ pragma solidity ^ 0.8.18;
 
 import "@openzeppelin/contracts@4.8.1/token/ERC20/ERC20.sol";
 
-/* this is an example ERC20 token called BOK */
+/* this is an example ERC20 token called TOK */
 
-contract BOK is ERC20 {
-    constructor() ERC20("BOK", "BOK") {
+contract TOK is ERC20 {
+    constructor() ERC20("TOK", "TOK") {
         _mint(msg.sender, 100000 * 10 ** decimals());
     }
 }
