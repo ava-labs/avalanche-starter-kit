ea94ef0f82e8905e15da5bb7c183708fe9733eeb
Sarp <sarp@satato.com>
2024-10-21 21:53:10 +0300

    update licence text

--- contracts/interchain-messaging/cross-chain-vrf-request-response/CrossChainVRFConsumer.sol
+++ contracts/interchain-messaging/cross-chain-vrf-request-response/CrossChainVRFConsumer.sol
@@ -1,4 +1,8 @@
-// SPDX-License-Identifier: MIT
+// (c) 2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// SPDX-License-Identifier: Ecosystem
+
 pragma solidity ^0.8.4;
 
 import "@chainlink/vrf/dev/interfaces/IVRFCoordinatorV2Plus.sol";
