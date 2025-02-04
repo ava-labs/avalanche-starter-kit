f7442210a174991f75f8e1c2372d1e2cbb404563
navillanueva <nicolasarnedo99@gmail.com>
2025-02-04 16:57:51 +0000

    fix: typo

--- contracts/interfaces/IVerifyBLS.sol
+++ contracts/interfaces/IVerifyBLS.sol
@@ -6,5 +6,5 @@ interface IVerifyBLS {
         view
         returns (bool result);
     function aggregatePublicKeys(bytes[] calldata publicKeys) external pure returns (bytes calldata);
-    function aggregateSingatures(bytes[] calldata signatures) external view returns (bytes calldata);
+    function aggregateSignatures(bytes[] calldata signatures) external view returns (bytes calldata);
 }
